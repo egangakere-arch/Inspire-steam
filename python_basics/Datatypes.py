e16f5d245de57db19c569292fb032f943270c87e
egangakere-arch <egangakere@gmail.com>
2026-02-11 09:48:43 +0300

    Program to add two numbers

---
+++ python_basics/Datatypes.py
@@ -0,0 +1,9 @@
+# Name : Gabriel Egan Gakere 
+# Date : 11/02/2026
+# Program to add two numbers 
+first_number = 12
+second_number = 86
+
+sum_number = first_number + second_number
+
+print(sum_number)
