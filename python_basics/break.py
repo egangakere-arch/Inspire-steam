37dcc376217055edcf399259b2bbf3c12dfcf63a
egangakere-arch <egangakere@gmail.com>
2026-02-17 09:46:16 +0300

    breaks and continue

---
+++ python_basics/break.py
@@ -0,0 +1,12 @@
+# Name : Gabriel Egan Gakere
+# Date : 17/02/2026
+# A program to illustrate break in python
+
+number = 1
+while number <10:
+    print(number)
+    if number == 4:
+        break
+    print("breaking from the loop")
+    continue
+
