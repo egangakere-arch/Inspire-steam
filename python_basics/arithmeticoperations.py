32b6fcef3b3079efcbe3207fbb69e7e2da303d98
egangakere-arch <egangakere@gmail.com>
2026-02-17 10:08:38 +0300

    arithmeic operations

---
+++ python_basics/arithmeticoperations.py
@@ -0,0 +1,31 @@
+# Name : Gabriel Egan Gakere
+# Date : 17/02/2026
+# A program to perfom arithmetic operations
+
+first_num = 12
+second_num = 34
+sum_num = first_num + second_num
+diff_num = first_num -second_num
+product_num = first_num * second_num
+quotient_num = first_num / second_num
+
+
+print("The sum of the numbers is %d"%sum_num)
+print("The difference of the numbers is %d"%diff_num)
+print("The product of the numbers is %d"%product_num)
+print("The quotient of the numbers is %d"%quotient_num)
+
+
+# Modulud - reminder
+print(7%5)
+
+
+
+# Even and odd numbers
+for x in range(0,21):
+    print(x)
+    if (x%2 == 1):
+        print("odd number")
+    elif  (x%2 == 0):
+        print("even number")
+
